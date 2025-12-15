27bc1d97a7b7e760b61b00c323a0cf5d993befde
The Typhothanian <thetyphothanian@gmail.com>
2025-12-15 11:58:06 -0700

    oops forgot queryValue for StencilMask

--- common/src/main/kotlin/net/typho/big_shot_lib/gl/state/StencilMask.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/gl/state/StencilMask.kt
@@ -1,9 +1,15 @@
 package net.typho.big_shot_lib.gl.state
 
+import org.lwjgl.opengl.GL11.GL_STENCIL_WRITEMASK
+import org.lwjgl.opengl.GL11.glGetInteger
 import org.lwjgl.opengl.GL20.glStencilMaskSeparate
 
 object StencilMask : GlState<StencilMask.Mask> {
     override fun default() = Mask.DEFAULT
+
+    override fun queryValue(): Mask? {
+        return Mask(glGetInteger(GL_STENCIL_WRITEMASK))
+    }
 
     override fun set(value: Mask) {
         glStencilMaskSeparate(value.face.id, value.mask)
