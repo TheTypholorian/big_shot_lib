851e32d7452dd6766b3b2ed60211729e7b3dcc4d
The Typhothanian <thetyphothanian@gmail.com>
2025-12-18 13:10:32 -0700

    bullshit

--- common/src/main/kotlin/net/typho/big_shot_lib/spirv/ShaderMixinCallback.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/spirv/ShaderMixinCallback.kt
@@ -19,15 +19,12 @@ interface ShaderMixinCallback {
         val options = shaderc_compile_options_initialize()
 
         init {
-            shaderc_compile_options_set_target_env(
-                options,
-                shaderc_target_env_opengl,
-                shaderc_env_version_opengl_4_5
-            )
-            shaderc_compile_options_set_source_language(
-                options,
-                shaderc_source_language_glsl
-            )
+            shaderc_compile_options_set_target_env(options, shaderc_target_env_opengl, shaderc_env_version_opengl_4_5)
+            shaderc_compile_options_set_source_language(options, shaderc_source_language_glsl)
+            shaderc_compile_options_set_auto_map_locations(options, true)
+            shaderc_compile_options_set_auto_bind_uniforms(options, true)
+            shaderc_compile_options_set_binding_base(options, shaderc_uniform_kind_buffer, 0)
+            shaderc_compile_options_set_binding_base(options, shaderc_uniform_kind_sampler, 0)
         }
 
         @JvmStatic
