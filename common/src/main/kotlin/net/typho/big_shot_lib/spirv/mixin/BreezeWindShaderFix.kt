abdb3c0754f5f80e32a726bd0c01ea54d3fdc2b5
The Typhothanian <thetyphothanian@gmail.com>
2025-12-28 11:40:42 -0700

    intellij messed up that refactor wth

--- common/src/main/kotlin/net/typho/big_shot_lib/spirv/mixin/BreezeWindShaderFix.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/spirv/mixin/BreezeWindShaderFix.kt
@@ -3,6 +3,7 @@ package net.typho.big_shot_lib.spirv.mixin
 import com.mojang.blaze3d.vertex.VertexFormat
 import net.minecraft.resources.ResourceLocation
 import net.typho.big_shot_lib.gl.resource.ShaderType
+import net.typho.big_shot_lib.spirv.ShaderMixinCallback
 
 object BreezeWindShaderFix : ShaderMixinCallback {
     override fun mixinGLSL(shader: ResourceLocation, type: ShaderType, format: VertexFormat?, code: String): String {
