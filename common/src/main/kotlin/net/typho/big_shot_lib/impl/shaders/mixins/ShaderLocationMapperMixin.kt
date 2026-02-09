6f90d94beb6ba67da082a1c8ceb969de07755162
The Typhothanian <thetyphothanian@gmail.com>
2026-02-09 09:03:31 -0700

    removed builtin shader mixins since they should be in the api

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/shaders/mixins/ShaderLocationMapperMixin.kt
+++
@@ -1,9 +0,0 @@
-package net.typho.big_shot_lib.impl.shaders.mixins
-
-import net.typho.big_shot_lib.api.shaders.ShaderProgramKey
-import net.typho.big_shot_lib.api.shaders.mixins.ShaderMixin
-
-object ShaderLocationMapperMixin : ShaderMixin.Factory {
-    override fun create(key: ShaderProgramKey): ShaderMixin {
-    }
-}
