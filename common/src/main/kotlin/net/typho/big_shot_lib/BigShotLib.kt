03cd129257a9f47cbf9161743fde3e80ee7447ba
The Typhothanian <thetyphothanian@gmail.com>
2026-02-09 13:33:36 -0700

    3.0.5

--- common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
@@ -5,24 +5,38 @@ import net.typho.big_shot_lib.api.shaders.mixins.ShaderBytecodeBuffer
 import net.typho.big_shot_lib.api.shaders.mixins.ShaderMixin
 import net.typho.big_shot_lib.api.shaders.mixins.ShaderMixinManager
 import net.typho.big_shot_lib.impl.shaders.mixins.BreezeWindShaderMixin
+import org.lwjgl.system.MemoryUtil
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 
 object BigShotLib {
     fun init() {
         ShaderMixinManager.register(BreezeWindShaderMixin)
-        ShaderMixinManager.register { key ->
-            println("Creating mixin for $key")
+        ShaderMixinManager.register(object : ShaderMixin {
+            override fun mixinBytecode(
+                key: ShaderSourceKey,
+                code: ShaderBytecodeBuffer
+            ): ShaderBytecodeBuffer {
+                println("Shader dump $key")
 
-            object : ShaderMixin {
-                override fun mixinPreCompile(key: ShaderSourceKey, code: String): String {
-                    println("\tPre compile size for ${key.type} is ${code.length} characters")
-                    return super.mixinPreCompile(key, code)
+                try {
+                    val arr = ByteArray(code.buffer.capacity() * 4)
+                    MemoryUtil.memByteBuffer(code.buffer).get(0, arr, 0, arr.size)
+                    val path =
+                        Paths.get("shader_dump", key.fileName().replace(":".toRegex(), "_") + ".bin")
+
+                    if (!Files.exists(path)) {
+                        Files.createFile(path)
+                    }
+
+                    Files.write(path, arr)
+                } catch (e: IOException) {
+                    throw RuntimeException(e)
                 }
 
-                override fun mixinBytecode(key: ShaderSourceKey, code: ShaderBytecodeBuffer): ShaderBytecodeBuffer {
-                    println("\tBytecode size for ${key.type} is ${code.buffer.capacity()} words")
-                    return super.mixinBytecode(key, code)
-                }
+                return code
             }
-        }
+        })
     }
 }
