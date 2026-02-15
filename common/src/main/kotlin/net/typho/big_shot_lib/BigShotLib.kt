6e7cab35b4994d5ce7dda1f8b6f7aef509dd5f4b
The Typhothanian <thetyphothanian@gmail.com>
2026-02-15 13:16:13 -0700

    removed that debug listener

--- common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
@@ -2,7 +2,6 @@ package net.typho.big_shot_lib
 
 import net.minecraft.resources.ResourceLocation
 import net.typho.big_shot_lib.api.client.rendering.shaders.mixins.ShaderMixinManager
-import net.typho.big_shot_lib.api.client.rendering.state.OpenGL
 import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
 import net.typho.big_shot_lib.impl.shaders.mixins.BreezeWindShaderMixin
 
@@ -16,10 +15,5 @@ object BigShotLib {
     @JvmStatic
     fun init() {
         ShaderMixinManager.register(BreezeWindShaderMixin)
-        OpenGL.INSTANCE.addDebugListener(object : OpenGL.DebugListener {
-            override fun accept(method: String, vararg args: Any) {
-                println("$method(${args.contentDeepToString()})")
-            }
-        })
     }
 }
