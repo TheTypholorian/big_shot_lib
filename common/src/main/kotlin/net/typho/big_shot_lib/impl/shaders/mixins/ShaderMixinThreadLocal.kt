1cc65b82e6d5b14fd4a72c1121335820810cd7ff
The Typhothanian <thetyphothanian@gmail.com>
2026-02-15 15:52:52 -0700

    changed exception in ShaderMixinThreadLocal to warning

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/shaders/mixins/ShaderMixinThreadLocal.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/shaders/mixins/ShaderMixinThreadLocal.kt
@@ -1,5 +1,6 @@
 package net.typho.big_shot_lib.impl.shaders.mixins
 
+import net.typho.big_shot_lib.api.BigShotApi
 import net.typho.big_shot_lib.api.client.rendering.shaders.ShaderProgramKey
 import net.typho.big_shot_lib.api.client.rendering.shaders.mixins.ShaderMixinManager
 
@@ -7,7 +8,7 @@ object ShaderMixinThreadLocal : ThreadLocal<ShaderMixinManager.Instance>() {
     @JvmStatic
     fun push(key: ShaderProgramKey) {
         if (get() != null) {
-            throw IllegalStateException()
+            BigShotApi.LOGGER.warn("Pushing ShaderMixinThreadLocal with $key while ${get()} is already bound, this might result in issues with shader mixins.")
         }
 
         set(ShaderMixinManager.create(key))
