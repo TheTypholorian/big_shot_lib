bf5c5fb8550f490b54f7bcda8af9725932e9d245
The Typhothanian <thetyphothanian@gmail.com>
2026-02-14 18:38:14 -0700

    I forgot some bind/unbinds in GlTexture2D, 3.4.3

--- src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/textures/GlTexture2D.kt
+++ src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/textures/GlTexture2D.kt
@@ -5,11 +5,15 @@ import net.typho.big_shot_lib.api.util.buffers.BufferUploader
 
 interface GlTexture2D : GlTexture, GlFramebufferAttachment {
     fun setInterpolation(min: InterpolationType, mag: InterpolationType = min) {
+        bind()
         OpenGL.INSTANCE.textureInterpolation(type(), min, mag)
+        unbind()
     }
 
     fun setWrapping(s: WrappingType, t: WrappingType = s) {
+        bind()
         OpenGL.INSTANCE.textureWrapping(type(), s, t)
+        unbind()
     }
 
     override fun resize(width: Int, height: Int): BufferUploader
