4d79be39dc1ed0dcad5e32f157463727c9d37473
The Typhothanian <thetyphothanian@gmail.com>
2026-02-15 15:50:27 -0700

    added sodium normals compat mixins

---
+++ common/src/main/java/net/typho/big_shot_lib/mixin/buffers/sodium/ChunkVertexEncoderVertexMixin.java
@@ -0,0 +1,22 @@
+package net.typho.big_shot_lib.mixin.buffers.sodium;
+
+import net.caffeinemc.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
+import net.typho.vibrancy.sodium.VertexWithNormal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(value = ChunkVertexEncoder.Vertex.class, remap = false)
+public class ChunkVertexEncoderVertexMixin implements VertexWithNormal {
+    @Unique
+    private int big_shot_lib$normal;
+
+    @Override
+    public int big_shot_lib$getNormal() {
+        return big_shot_lib$normal;
+    }
+
+    @Override
+    public void big_shot_lib$setNormal(int n) {
+        big_shot_lib$normal = n;
+    }
+}
