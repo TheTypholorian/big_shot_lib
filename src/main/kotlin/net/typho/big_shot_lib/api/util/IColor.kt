f8d24e8594106c99644bace0721cf57a56705072
The Typhothanian <thetyphothanian@gmail.com>
2026-02-15 19:01:44 -0700

    IColor tweaks

--- src/main/kotlin/net/typho/big_shot_lib/api/util/IColor.kt
+++ src/main/kotlin/net/typho/big_shot_lib/api/util/IColor.kt
@@ -14,55 +14,55 @@ interface IColor {
         val FULL_OFF = RGBAF(0f, 0f, 0f, 0f)
 
         @JvmField
-        val WHITE = from(Color.WHITE)
+        val WHITE = RGBA(Color.WHITE)
         @JvmField
-        val LIGHT_GRAY = from(Color.LIGHT_GRAY)
+        val LIGHT_GRAY = RGBA(Color.LIGHT_GRAY)
         @JvmField
-        val GRAY = from(Color.GRAY)
+        val GRAY = RGBA(Color.GRAY)
         @JvmField
-        val DARK_GRAY = from(Color.DARK_GRAY)
+        val DARK_GRAY = RGBA(Color.DARK_GRAY)
         @JvmField
-        val BLACK = from(Color.BLACK)
+        val BLACK = RGBA(Color.BLACK)
         @JvmField
-        val RED = from(Color.RED)
+        val RED = RGBA(Color.RED)
         @JvmField
-        val PINK = from(Color.PINK)
+        val PINK = RGBA(Color.PINK)
         @JvmField
-        val ORANGE = from(Color.ORANGE)
+        val ORANGE = RGBA(Color.ORANGE)
         @JvmField
-        val YELLOW = from(Color.YELLOW)
+        val YELLOW = RGBA(Color.YELLOW)
         @JvmField
-        val GREEN = from(Color.GREEN)
+        val GREEN = RGBA(Color.GREEN)
         @JvmField
-        val MAGENTA = from(Color.MAGENTA)
+        val MAGENTA = RGBA(Color.MAGENTA)
         @JvmField
-        val CYAN = from(Color.CYAN)
+        val CYAN = RGBA(Color.CYAN)
         @JvmField
-        val BLUE = from(Color.BLUE)
+        val BLUE = RGBA(Color.BLUE)
 
         @JvmField
         val CODEC_PACKED: Codec<IColor> = Codec.INT.xmap(
-            { vec -> from(vec) },
+            { vec -> RGBA(vec) },
             { color -> color.toRGBA() }
         )
         @JvmField
         val CODEC_3I: Codec<IColor> = NeoCodecs.VEC3I.xmap(
-            { vec -> from(vec) },
+            { vec -> RGB(vec) },
             { color -> color.toVec3() }
         )
         @JvmField
         val CODEC_4I: Codec<IColor> = NeoCodecs.VEC4I.xmap(
-            { vec -> from(vec) },
+            { vec -> RGBA(vec) },
             { color -> color.toVec4() }
         )
         @JvmField
         val CODEC_3F: Codec<IColor> = NeoCodecs.VEC3F.xmap(
-            { vec -> from(vec) },
+            { vec -> RGBF(vec) },
             { color -> color.toVec3F() }
         )
         @JvmField
         val CODEC_4F: Codec<IColor> = NeoCodecs.VEC4F.xmap(
-            { vec -> from(vec) },
+            { vec -> RGBAF(vec) },
             { color -> color.toVec4F() }
         )
         @JvmField
@@ -82,18 +82,6 @@ interface IColor {
             { either -> either.map({ l -> l }, { r -> r.map({ l -> l }, { r1 -> r1.map({ l -> l }, { r2 -> r2.map({ l -> l }, { r3 -> r3 }) }) }) }) },
             { color -> Either.left(color) }
         )
-
-        fun from(argb: Int) = RGBA(argb shl 16 and 0xFF, argb shl 8 and 0xFF, argb and 0xFF, argb ushr 24)
-
-        fun from(color: Color) = RGBA(color.red, color.green, color.blue, color.alpha)
-
-        fun from(color: Vector3ic) = RGB(color.x(), color.y(), color.z())
-
-        fun from(color: Vector4ic) = RGBA(color.x(), color.y(), color.z(), color.w())
-
-        fun from(color: Vector3fc) = RGBF(color.x(), color.y(), color.z())
-
-        fun from(color: Vector4fc) = RGBAF(color.x(), color.y(), color.z(), color.w())
     }
 
     fun redF(): Float = red() / 255f
@@ -122,6 +110,10 @@ interface IColor {
 
     fun toRGBA() = ((alpha() ?: 0xFF) shl 24) or (red() shl 16) or (green() shl 8) or blue()
 
+    fun toIntArray() = intArrayOf(red(), green(), blue(), alpha() ?: 255)
+
+    fun toFloatArray() = floatArrayOf(redF(), greenF(), blueF(), alphaF() ?: 1f)
+
     @JvmRecord
     data class RGB(
         @JvmField
@@ -131,6 +123,18 @@ interface IColor {
         @JvmField
         val blue: Int
     ) : IColor {
+        constructor(rgb: Int) : this(rgb shl 16 and 0xFF, rgb shl 8 and 0xFF, rgb and 0xFF)
+
+        constructor(color: Color) : this(color.red, color.green, color.blue)
+
+        constructor(color: Vector3ic) : this(color.x(), color.y(), color.z())
+
+        constructor(color: Vector3fc) : this((color.x() * 255).toInt(), (color.y() * 255).toInt(), (color.z() * 255).toInt())
+
+        constructor(color: IntArray) : this(color[0], color[1], color[2])
+
+        constructor(color: FloatArray) : this((color[0] * 255).toInt(), (color[1] * 255).toInt(), (color[2] * 255).toInt())
+
         override fun red() = red
 
         override fun green() = green
@@ -151,6 +155,18 @@ interface IColor {
         @JvmField
         val alpha: Int
     ) : IColor {
+        constructor(argb: Int) : this(argb shl 16 and 0xFF, argb shl 8 and 0xFF, argb and 0xFF, argb ushr 24)
+
+        constructor(color: Color) : this(color.red, color.green, color.blue, color.alpha)
+
+        constructor(color: Vector4ic) : this(color.x(), color.y(), color.z(), color.w())
+
+        constructor(color: Vector4fc) : this((color.x() * 255).toInt(), (color.y() * 255).toInt(), (color.z() * 255).toInt(), (color.w() * 255).toInt())
+
+        constructor(color: IntArray) : this(color[0], color[1], color[2], color[3])
+
+        constructor(color: FloatArray) : this((color[0] * 255).toInt(), (color[1] * 255).toInt(), (color[2] * 255).toInt(), (color[3] * 255).toInt())
+
         override fun red() = red
 
         override fun green() = green
@@ -169,6 +185,18 @@ interface IColor {
         @JvmField
         val blue: Float
     ) : IColor {
+        constructor(rgb: Int) : this((rgb shl 16 and 0xFF) / 255f, (rgb shl 8 and 0xFF) / 255f, (rgb and 0xFF) / 255f)
+
+        constructor(color: Color) : this(color.red / 255f, color.green / 255f, color.blue / 255f)
+
+        constructor(color: Vector3ic) : this(color.x() / 255f, color.y() / 255f, color.z() / 255f)
+
+        constructor(color: Vector3fc) : this(color.x(), color.y(), color.z())
+
+        constructor(color: IntArray) : this(color[0] / 255f, color[1] / 255f, color[2] / 255f)
+
+        constructor(color: FloatArray) : this(color[0], color[1], color[2])
+
         override fun redF() = red
 
         override fun greenF() = green
@@ -189,6 +217,18 @@ interface IColor {
         @JvmField
         val alpha: Float
     ) : IColor {
+        constructor(argb: Int) : this((argb shl 16 and 0xFF) / 255f, (argb shl 8 and 0xFF) / 255f, (argb and 0xFF) / 255f, (argb ushr 24) / 255f)
+
+        constructor(color: Color) : this(color.red / 255f, color.green / 255f, color.blue / 255f, color.alpha / 255f)
+
+        constructor(color: Vector4ic) : this(color.x() / 255f, color.y() / 255f, color.z() / 255f, color.w() / 255f)
+
+        constructor(color: Vector4fc) : this(color.x(), color.y(), color.z(), color.w())
+
+        constructor(color: IntArray) : this(color[0] / 255f, color[1] / 255f, color[2] / 255f, color[3] / 255f)
+
+        constructor(color: FloatArray) : this(color[0], color[1], color[2], color[3])
+
         override fun redF() = red
 
         override fun greenF() = green
