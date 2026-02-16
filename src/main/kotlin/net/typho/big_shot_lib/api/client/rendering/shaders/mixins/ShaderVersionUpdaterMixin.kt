cc9577abf7ea38aa7f77a9372fec8fb57b146e84
The Typhothanian <thetyphothanian@gmail.com>
2026-02-15 19:21:16 -0700

    fixed ShaderVersionUpdaterMixin and fabric.mod.json

--- src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/shaders/mixins/ShaderVersionUpdaterMixin.kt
+++ src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/shaders/mixins/ShaderVersionUpdaterMixin.kt
@@ -1,42 +1,45 @@
 package net.typho.big_shot_lib.api.client.rendering.shaders.mixins
 
+import net.typho.big_shot_lib.api.client.rendering.shaders.ShaderProgramKey
 import net.typho.big_shot_lib.api.client.rendering.shaders.ShaderSourceKey
 
-object ShaderVersionUpdaterMixin : ShaderMixin {
+object ShaderVersionUpdaterMixin : ShaderMixin.Factory {
     const val SPIRV_VERSION = 450
     const val SPIRV_VERSION_STRING = "$SPIRV_VERSION core"
-    const val GLSL_VERSION = 150
-    const val GLSL_VERSION_STRING = "$SPIRV_VERSION core"
 
-    override fun mixinPreCompile(key: ShaderSourceKey, code: String): String {
-        if (code.startsWith("#version")) {
-            val version = code.split("\\s+".toRegex())
-                .dropLastWhile { it.isEmpty() }
-                .toTypedArray()[1].toInt()
-
-            if (version < SPIRV_VERSION) {
-                return "#version $SPIRV_VERSION_STRING" + code.substring(code.indexOf('\n'))
-            }
-        } else {
-            return "#version $SPIRV_VERSION_STRING\n$code"
-        }
-
-        return code
+    override fun create(
+        key: ShaderProgramKey,
+        parent: ShaderMixinManager.Instance
+    ): ShaderMixin {
+        return Mixin()
     }
 
-    override fun mixinPostCompile(key: ShaderSourceKey, code: String): String {
-        if (code.startsWith("#version")) {
-            val version = code.split("\\s+".toRegex())
-                .dropLastWhile { it.isEmpty() }
-                .toTypedArray()[1].toInt()
+    class Mixin : ShaderMixin {
+        var version: Int? = null
 
-            if (version < GLSL_VERSION) {
-                return "#version $GLSL_VERSION_STRING" + code.substring(code.indexOf('\n'))
+        override fun mixinPreCompile(key: ShaderSourceKey, code: String): String {
+            if (code.startsWith("#version")) {
+                val version = code.split("\\s+".toRegex())
+                    .dropLastWhile { it.isEmpty() }
+                    .toTypedArray()[1].toInt()
+                this.version = version
+
+                if (version < SPIRV_VERSION) {
+                    return "#version $SPIRV_VERSION_STRING" + code.substring(code.indexOf('\n'))
+                }
+            } else {
+                return "#version $SPIRV_VERSION_STRING\n$code"
             }
-        } else {
-            return "#version $GLSL_VERSION_STRING\n$code"
+
+            return code
         }
 
-        return code
+        override fun mixinPostCompile(key: ShaderSourceKey, code: String): String {
+            if (code.startsWith("#version")) {
+                return "#version $version" + code.substring(code.indexOf('\n'))
+            }
+
+            return code
+        }
     }
 }
