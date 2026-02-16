d0667bc78bd467504aa0a4dace971a1a2e3903aa
The Typhothanian <thetyphothanian@gmail.com>
2026-02-16 16:18:28 -0700

    fixed client chunk cache mixin

--- common/src/main/java/net/typho/big_shot_lib/mixin/event/ClientChunkCacheMixin.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/event/ClientChunkCacheMixin.java
@@ -22,7 +22,7 @@ public class ClientChunkCacheMixin {
             method = "drop",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/multiplayer/ClientChunkCache$Storage;replace(ILnet/minecraft/world/level/chunk/LevelChunk;Lnet/minecraft/world/level/chunk/LevelChunk;)Lnet/minecraft/world/level/chunk/LevelChunk;"
+                    target = "Lnet/minecraft/client/multiplayer/ClientChunkCache$Storage;drop(ILnet/minecraft/world/level/chunk/LevelChunk;)V"
             )
     )
     private void drop(
