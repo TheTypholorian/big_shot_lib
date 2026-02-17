544c63f5171e30b0a2d36bc3c386461708a6848b
The Typhothanian <thetyphothanian@gmail.com>
2026-02-17 15:00:37 -0700

    neoforge is stupid and made me use their event for registering resource listeners

--- fabric/src/main/kotlin/net/typho/big_shot_lib/BigShotLibFabric.kt
+++ fabric/src/main/kotlin/net/typho/big_shot_lib/BigShotLibFabric.kt
@@ -1,9 +1,44 @@
 package net.typho.big_shot_lib
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.server.packs.PackType
+import net.minecraft.server.packs.resources.PreparableReloadListener
+import net.minecraft.server.packs.resources.ResourceManager
+import net.minecraft.util.profiling.ProfilerFiller
+import net.typho.big_shot_lib.BigShotLib.toMojang
+import net.typho.big_shot_lib.api.BigShotApi
+import net.typho.big_shot_lib.api.client.rendering.shaders.NeoShaderRegistry
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
 
 object BigShotLibFabric : ClientModInitializer {
     override fun onInitializeClient() {
         BigShotLib.init()
+        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(object : IdentifiableResourceReloadListener {
+            override fun getFabricId(): ResourceLocation {
+                return BigShotApi.id("shaders").toMojang()
+            }
+
+            override fun reload(
+                preparationBarrier: PreparableReloadListener.PreparationBarrier,
+                resourceManager: ResourceManager,
+                preparationsProfiler: ProfilerFiller,
+                reloadProfiler: ProfilerFiller,
+                backgroundExecutor: Executor,
+                gameExecutor: Executor
+            ): CompletableFuture<Void> {
+                return NeoShaderRegistry.reload(
+                    preparationBarrier,
+                    resourceManager,
+                    preparationsProfiler,
+                    reloadProfiler,
+                    backgroundExecutor,
+                    gameExecutor
+                )
+            }
+        })
     }
 }
