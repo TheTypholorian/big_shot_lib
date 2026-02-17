a9ce7958ec7cef92a0ddf7537f51aff86af254d7
The Typhothanian <thetyphothanian@gmail.com>
2026-02-17 06:47:58 -0700

    added texture stack rebind to TextureUtilImpl.getMinecraftTexture

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/util/TextureUtilImpl.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/util/TextureUtilImpl.kt
@@ -7,9 +7,11 @@ import net.minecraft.world.inventory.InventoryMenu
 import net.typho.big_shot_lib.BigShotLib.toMojang
 import net.typho.big_shot_lib.BigShotLib.toNeo
 import net.typho.big_shot_lib.api.client.rendering.services.TextureUtil
+import net.typho.big_shot_lib.api.client.rendering.state.GlStateStack
 import net.typho.big_shot_lib.api.client.rendering.textures.GlTexture2D
 import net.typho.big_shot_lib.api.client.rendering.textures.NeoTexture2D
 import net.typho.big_shot_lib.api.client.rendering.textures.TextureFormat
+import net.typho.big_shot_lib.api.client.rendering.textures.TextureType
 import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
 import org.lwjgl.opengl.GL11.*
 import kotlin.jvm.optionals.getOrNull
@@ -34,6 +36,7 @@ class TextureUtilImpl : TextureUtil {
         return textures.computeIfAbsent(Minecraft.getInstance().textureManager.getTexture(texture.toMojang())) { texture ->
             texture.bind()
             val format = glGetTexLevelParameteri(GL_TEXTURE_2D, 0, GL_TEXTURE_INTERNAL_FORMAT)
+            GlStateStack.textures[TextureType.TEXTURE_2D]?.rebind()
             return@computeIfAbsent NeoTexture2D(texture.id, TextureFormat.entries.first { it.internalId == format }, false)
         }
     }
