9dbc77df13beead7f270c533e38df210552158fa
The Typhothanian <thetyphothanian@gmail.com>
2026-02-16 17:21:10 -0700

    NeoRegistry impl

--- common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/BigShotLib.kt
@@ -1,7 +1,11 @@
 package net.typho.big_shot_lib
 
+import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.TagKey
 import net.typho.big_shot_lib.api.client.rendering.shaders.mixins.ShaderMixinManager
+import net.typho.big_shot_lib.api.util.resources.NeoResourceKey
+import net.typho.big_shot_lib.api.util.resources.NeoTagKey
 import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
 import net.typho.big_shot_lib.impl.shaders.mixins.BreezeWindShaderMixin
 
@@ -11,6 +15,18 @@ object BigShotLib {
 
     @JvmStatic
     fun ResourceIdentifier.toMojang(): ResourceLocation = ResourceLocation.fromNamespaceAndPath(namespace, path)
+
+    @JvmStatic
+    fun <T> ResourceKey<T>.toNeo(): NeoResourceKey<T> = NeoResourceKey(registry().toNeo(), location().toNeo())
+
+    @JvmStatic
+    fun <T> NeoResourceKey<T>.toMojang(): ResourceKey<T> = ResourceKey.create(ResourceKey.createRegistryKey(registry.toMojang()), location.toMojang())
+
+    @JvmStatic
+    fun <T> TagKey<T>.toNeo(): NeoTagKey<T> = NeoTagKey(registry().toNeo().location, location().toNeo())
+
+    @JvmStatic
+    fun <T> NeoTagKey<T>.toMojang(): TagKey<T> = TagKey.create(ResourceKey.createRegistryKey(registry.toMojang()), location.toMojang())
 
     @JvmStatic
     fun init() {
