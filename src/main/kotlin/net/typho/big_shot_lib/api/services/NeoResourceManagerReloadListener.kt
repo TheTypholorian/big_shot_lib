087e200fccb2f61e755977a99295eaac9d1478c8
The Typhothanian <thetyphothanian@gmail.com>
2026-02-19 12:32:06 -0700

    made NeoResourceManagerReloadListener not extend ResourceManagerReloadListener due to mapping issues

--- src/main/kotlin/net/typho/big_shot_lib/api/services/NeoResourceManagerReloadListener.kt
+++ src/main/kotlin/net/typho/big_shot_lib/api/services/NeoResourceManagerReloadListener.kt
@@ -1,12 +1,5 @@
 package net.typho.big_shot_lib.api.services
 
-import net.minecraft.server.packs.resources.ResourceManager
-import net.minecraft.server.packs.resources.ResourceManagerReloadListener
-
-interface NeoResourceManagerReloadListener : ResourceManagerReloadListener {
-    override fun onResourceManagerReload(manager: ResourceManager) {
-        onResourceManagerReload(WrapperUtil.INSTANCE.wrap(manager))
-    }
-
+interface NeoResourceManagerReloadListener {
     fun onResourceManagerReload(manager: NeoResourceManager)
 }
