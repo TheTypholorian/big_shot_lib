586c0262b93d0d17f429c2efeb55144bbb2a9bff
The Typhothanian <thetyphothanian@gmail.com>
2026-02-18 21:21:49 -0700

    added enableBlend/disableBlend methods to DynamicBufferRegistry, 3.7.1

--- src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/buffers/DynamicBufferRegistry.kt
+++ src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/buffers/DynamicBufferRegistry.kt
@@ -1,5 +1,9 @@
 package net.typho.big_shot_lib.api.client.rendering.buffers
 
+import org.lwjgl.opengl.GL11.GL_BLEND
+import org.lwjgl.opengl.GL30.glDisablei
+import org.lwjgl.opengl.GL30.glEnablei
+
 object DynamicBufferRegistry {
     @JvmField
     val buffers = HashMap<Int, DynamicBuffer>()
@@ -14,6 +18,24 @@ object DynamicBufferRegistry {
         val location = buffers.size + 1
         buffers[location] = buffer
         buffer.setShaderLocation(location)
+    }
+
+    @JvmStatic
+    fun enableBlend() {
+        for (entry in buffers) {
+            if (entry.value.blend()) {
+                glEnablei(GL_BLEND, entry.key)
+            } else {
+                glDisablei(GL_BLEND, entry.key)
+            }
+        }
+    }
+
+    @JvmStatic
+    fun disableBlend() {
+        for (entry in buffers) {
+            glDisablei(GL_BLEND, entry.key)
+        }
     }
 
     fun init() = Unit
