b5ebb7832de0383ca7b0dc7d5f6c9139b84c3941
The Typhothanian <thetyphothanian@gmail.com>
2026-02-19 12:37:47 -0700

    3.7.2

--- fabric/src/main/kotlin/net/typho/big_shot_lib/BigShotLibFabric.kt
+++ fabric/src/main/kotlin/net/typho/big_shot_lib/BigShotLibFabric.kt
@@ -1,43 +1,26 @@
 package net.typho.big_shot_lib
 
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.PackType
-import net.minecraft.server.packs.resources.PreparableReloadListener
 import net.minecraft.server.packs.resources.ResourceManager
-import net.minecraft.util.profiling.ProfilerFiller
 import net.typho.big_shot_lib.BigShotLib.toMojang
 import net.typho.big_shot_lib.api.BigShotApi
 import net.typho.big_shot_lib.api.client.rendering.shaders.NeoShaderRegistry
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executor
+import net.typho.big_shot_lib.api.services.WrapperUtil
 
 object BigShotLibFabric : ClientModInitializer {
     override fun onInitializeClient() {
         BigShotLib.init()
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(object : IdentifiableResourceReloadListener {
+        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(object : SimpleSynchronousResourceReloadListener {
             override fun getFabricId(): ResourceLocation {
                 return BigShotApi.id("shaders").toMojang()
             }
 
-            override fun reload(
-                preparationBarrier: PreparableReloadListener.PreparationBarrier,
-                resourceManager: ResourceManager,
-                preparationsProfiler: ProfilerFiller,
-                reloadProfiler: ProfilerFiller,
-                backgroundExecutor: Executor,
-                gameExecutor: Executor
-            ): CompletableFuture<Void> {
-                return NeoShaderRegistry.reload(
-                    preparationBarrier,
-                    resourceManager,
-                    preparationsProfiler,
-                    reloadProfiler,
-                    backgroundExecutor,
-                    gameExecutor
-                )
+            override fun onResourceManagerReload(resourceManager: ResourceManager) {
+                NeoShaderRegistry.onResourceManagerReload(WrapperUtil.INSTANCE.wrap(resourceManager))
             }
         })
     }
