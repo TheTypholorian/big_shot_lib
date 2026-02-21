1824a5a92be39f948496aecbe10b22498a17d9d3
The Typhothanian <thetyphothanian@gmail.com>
2026-02-20 19:27:48 -0700

    fixed neoforge class

--- neoforge/src/main/kotlin/net/typho/big_shot_lib/BigShotLibNeoForge.kt
+++ neoforge/src/main/kotlin/net/typho/big_shot_lib/BigShotLibNeoForge.kt
@@ -7,7 +7,8 @@ import net.neoforged.api.distmarker.OnlyIn
 import net.neoforged.bus.api.IEventBus
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
-import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent
+import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent
+import net.typho.big_shot_lib.BigShotLib.toMojang
 import net.typho.big_shot_lib.api.BigShotApi
 import net.typho.big_shot_lib.api.client.rendering.shaders.NeoShaderRegistry
 import net.typho.big_shot_lib.api.services.WrapperUtil
@@ -17,8 +18,8 @@ import net.typho.big_shot_lib.api.services.WrapperUtil
 class BigShotLibNeoForge(eventBus: IEventBus, modContainer: ModContainer) {
     init {
         BigShotLib.init()
-        eventBus.addListener { event: RegisterClientReloadListenersEvent ->
-            event.registerReloadListener(object : ResourceManagerReloadListener {
+        eventBus.addListener { event: AddClientReloadListenersEvent ->
+            event.addListener(BigShotApi.id("shaders").toMojang(), object : ResourceManagerReloadListener {
                 override fun onResourceManagerReload(resourceManager: ResourceManager) {
                     NeoShaderRegistry.onResourceManagerReload(WrapperUtil.INSTANCE.wrap(resourceManager))
                 }
