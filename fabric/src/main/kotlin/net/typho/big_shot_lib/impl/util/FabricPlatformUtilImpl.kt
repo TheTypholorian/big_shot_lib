b7d9daba7a1161238f52fec5f9c4311c133e2333
The Typhothanian <thetyphothanian@gmail.com>
2026-02-21 14:52:15 -0700

    yay 3.9.0 is implemented

---
+++ fabric/src/main/kotlin/net/typho/big_shot_lib/impl/util/FabricPlatformUtilImpl.kt
@@ -0,0 +1,57 @@
+package net.typho.big_shot_lib.impl.util
+
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.metadata.CustomValue
+import net.typho.big_shot_lib.api.services.ModContainer
+import net.typho.big_shot_lib.api.services.PlatformUtil
+import net.typho.big_shot_lib.api.util.ModLoader
+import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
+
+class FabricPlatformUtilImpl : PlatformUtil {
+    override val loader = ModLoader.FABRIC
+    override val mods: Collection<ModContainer>
+        get() = FabricLoader.getInstance().allMods.map { ModContainerImpl(it) }
+
+    @JvmRecord
+    data class ModContainerImpl(
+        @JvmField
+        val inner: net.fabricmc.loader.api.ModContainer
+    ) : ModContainer {
+        override val modId: String
+            get() = inner.metadata.id
+        override val displayName: String
+            get() = inner.metadata.name
+        override val description: String
+            get() = inner.metadata.description
+        override val version: String
+            get() = inner.metadata.version.friendlyString
+        override val customData: Map<String, Any?>
+            get() = customValueMapToNormalMap(inner.metadata.customValues)
+
+        override fun <T> loadEntrypoint(id: ResourceIdentifier, cls: Class<T>): T? {
+            return FabricLoader.getInstance()
+                .getEntrypointContainers(id.toString(), cls)
+                .firstOrNull { it.provider == inner }
+                ?.entrypoint
+        }
+
+        companion object {
+            @JvmStatic
+            fun customValueToNormalValue(value: CustomValue): Any? {
+                return when (value.type) {
+                    CustomValue.CvType.OBJECT -> customValueMapToNormalMap(value.asObject)
+                    CustomValue.CvType.ARRAY -> value.asArray.map(::customValueToNormalValue)
+                    CustomValue.CvType.STRING -> value.toString()
+                    CustomValue.CvType.NUMBER -> value.asNumber
+                    CustomValue.CvType.BOOLEAN -> value.asBoolean
+                    CustomValue.CvType.NULL -> null
+                }
+            }
+
+            @JvmStatic
+            fun customValueMapToNormalMap(map: Iterable<Map.Entry<String, CustomValue>>): Map<String, Any?> {
+                return map.associate { it.key to customValueToNormalValue(it.value) }
+            }
+        }
+    }
+}
---
+++ neoforge/src/main/kotlin/net/typho/big_shot_lib/impl/util/NeoForgePlatformUtilImpl.kt
@@ -0,0 +1,52 @@
+package net.typho.big_shot_lib.impl.util
+
+import net.neoforged.fml.ModList
+import net.typho.big_shot_lib.api.services.ModContainer
+import net.typho.big_shot_lib.api.services.PlatformUtil
+import net.typho.big_shot_lib.api.util.ModLoader
+import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
+import kotlin.reflect.full.companionObjectInstance
+
+class NeoForgePlatformUtilImpl : PlatformUtil {
+    override val loader = ModLoader.NEOFORGE
+    override val mods: Collection<ModContainer>
+        get() = ModList.get().sortedMods.map { ModContainerImpl(it) }
+
+    @JvmRecord
+    data class ModContainerImpl(
+        @JvmField
+        val inner: net.neoforged.fml.ModContainer
+    ) : ModContainer {
+        override val modId: String
+            get() = inner.modId
+        override val displayName: String
+            get() = inner.modInfo.displayName
+        override val description: String
+            get() = inner.modInfo.description
+        override val version: String
+            get() = inner.modInfo.version.toString()
+        override val customData: Map<String, Any>
+            get() = inner.modInfo.modProperties
+
+        override fun <T> loadEntrypoint(id: ResourceIdentifier): T? {
+            val entrypoints = inner.modInfo.modProperties["entrypoints"] as? Map<*, *> ?: return null
+            val name = entrypoints[id.toString()] as? String ?: return null
+
+            val cls = Class.forName(name, true, ModContainerImpl::class.java.classLoader)
+
+            if (cls.kotlin.objectInstance is T) {
+                return cls.kotlin.objectInstance as T
+            }
+
+            if (cls.kotlin.companionObjectInstance is T) {
+                return cls.kotlin.companionObjectInstance as T
+            }
+
+            cls.kotlin.constructors.firstOrNull { it.parameters.isEmpty() }?.let { return it.call() as T }
+
+            val constructor = cls.getDeclaredConstructor()
+            constructor.isAccessible = true
+            return constructor.newInstance() as T
+        }
+    }
+}
