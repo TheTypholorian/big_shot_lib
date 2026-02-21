7476ef6a9a99388208079e9d27abb707fd21cbb1
The Typhothanian <thetyphothanian@gmail.com>
2026-02-21 10:30:11 -0700

    3.9.0

--- common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
+++ common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
@@ -2,8 +2,9 @@
 
 uniform float FogStart;
 uniform float FogEnd;
+uniform int FogShape;
 uniform vec4 FogColor;
 
-vec4 big_shot_fog(vec4 input, vec3 delta) {
-
+vec4 bigShotFog(vec4 input, vec3 delta) {
+    return linear_fog(input, fog_distance(delta, FogShape), FogStart, FogEnd, FogColor);
 }
