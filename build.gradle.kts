2dbd42fcc78691b1928947e6270e09020090c0a8
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 06:23:49 -0700

    build.gradle fix and class cast fix in BigShotLibFabric

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,3 @@ plugins {
     // see https://projects.neoforged.net/neoforged/moddevgradle for new versions
     alias(libs.plugins.moddev) apply false
 }
-
-subprojects {
-    repositories {
-        ivy {
-            url = uri("https://github.com/TheTypholorian/")
-            patternLayout {
-                artifact("[organisation]/releases/download/[revision]/[artifact]-[revision](-[classifier]).[ext]")
-            }
-            metadataSources {
-                artifact()
-            }
-        }
-    }
-}
