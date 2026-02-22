1d5aae59140e0a0e7ad9cdda51d44313b0ced215
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 07:47:33 -0700

    3.9.2

---
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/state/FogUtilImpl.kt
@@ -0,0 +1,16 @@
+package net.typho.big_shot_lib.impl.state
+
+import com.mojang.blaze3d.systems.RenderSystem
+import net.typho.big_shot_lib.api.client.rendering.shaders.GlShader
+import net.typho.big_shot_lib.api.client.rendering.state.FogUtil
+import net.typho.big_shot_lib.api.util.IColor
+
+class FogUtilImpl : FogUtil {
+    override fun upload(shader: GlShader) {
+        val mojang = RenderSystem.getShaderFog()
+        shader.getUniform("FogStart")?.setValue(mojang.start)
+        shader.getUniform("FogEnd")?.setValue(mojang.end)
+        shader.getUniform("FogShape")?.setValue(mojang.shape.index)
+        shader.getUniform("FogColor")?.setValue(IColor.RGBAF(mojang.red, mojang.green, mojang.blue, mojang.alpha))
+    }
+}
