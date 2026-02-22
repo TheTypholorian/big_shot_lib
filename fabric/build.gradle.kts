0105691c58c2006a43cbcd2c8d3759b2ebd161e3
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 12:15:54 -0700

    fixed modrinth versions

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -23,12 +23,12 @@ modrinth {
     }
 
     projectId = "big-shot-lib"
-    versionName = "$modName $version for Fabric 1.21.5"
-    versionNumber = "mc1.21.5-$version-fabric"
+    versionName = "$modName $version for Fabric 1.21.10"
+    versionNumber = "mc1.21.10-$version-fabric"
     versionType = "release"
     uploadFile.set(tasks.remapJar)
     additionalFiles.add(tasks.remapSourcesJar)
-    gameVersions.addAll("1.21.5")
+    gameVersions.addAll("1.21.10")
     loaders.add("fabric")
 
     dependencies {
