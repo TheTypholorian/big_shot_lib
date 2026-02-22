0581f7e9e8d0d053ed1801a5de859d50ffaac7fb
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 08:51:39 -0700

    fixed events

--- fabric/src/main/java/net/typho/big_shot_lib/mixin/fabric/event/ChunkMapMixin.java
+++ fabric/src/main/java/net/typho/big_shot_lib/mixin/fabric/event/ChunkMapMixin.java
@@ -19,7 +19,7 @@ import java.util.concurrent.CompletableFuture;
 public class ChunkMapMixin {
     @Shadow
     @Final
-    private ServerLevel level;
+    ServerLevel level;
 
     @Inject(
             method = "method_60440",
