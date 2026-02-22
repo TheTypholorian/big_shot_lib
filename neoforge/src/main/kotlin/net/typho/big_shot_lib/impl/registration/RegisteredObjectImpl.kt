a43ba49fd3016967feaa13c2f53cd1e6f7bf91e8
The Typhothanian <thetyphothanian@gmail.com>
2026-02-21 19:49:45 -0700

    3.9.1 yay

---
+++ neoforge/src/main/kotlin/net/typho/big_shot_lib/impl/registration/RegisteredObjectImpl.kt
@@ -0,0 +1,19 @@
+package net.typho.big_shot_lib.impl.registration
+
+import net.minecraft.core.Registry
+import net.neoforged.neoforge.registries.DeferredHolder
+import net.typho.big_shot_lib.api.registration.RegisteredObject
+import net.typho.big_shot_lib.api.util.resources.NeoResourceKey
+import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
+
+@JvmRecord
+data class RegisteredObjectImpl<T>(
+    override val registry: NeoResourceKey<Registry<T>>,
+    override val key: ResourceIdentifier,
+    @JvmField
+    val holder: DeferredHolder<*, T>
+) : RegisteredObject<T> {
+    override fun get(): T = holder.get()
+
+    override fun isRegistered() = holder.isBound
+}
