90d5da2ea2ac1b40ffab82a43c89fd1f02b4765d
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 08:51:39 -0700

    fixed events

--- common/src/main/java/net/typho/big_shot_lib/mixin/event/ClientChunkCacheMixin.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/event/ClientChunkCacheMixin.java
@@ -2,13 +2,16 @@ package net.typho.big_shot_lib.mixin.event;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.multiplayer.ClientChunkCache;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.typho.big_shot_lib.BigShotCommonEventStorage;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,6 +22,10 @@ import java.util.function.Consumer;
 
 @Mixin(ClientChunkCache.class)
 public class ClientChunkCacheMixin {
+    @Shadow
+    @Final
+    ClientLevel level;
+
     @Inject(
             method = "drop",
             at = @At(
@@ -31,7 +38,7 @@ public class ClientChunkCacheMixin {
             CallbackInfo ci,
             @Local LevelChunk chunk
     ) {
-        BigShotCommonEventStorage.onChunkChanged.forEach(event -> event.invoke(chunk, null));
+        BigShotCommonEventStorage.onChunkChanged.forEach(event -> event.invoke(level, chunk, null));
     }
 
     @Inject(
@@ -47,6 +54,6 @@ public class ClientChunkCacheMixin {
             CallbackInfoReturnable<LevelChunk> cir,
             @Local LevelChunk chunk
     ) {
-        BigShotCommonEventStorage.onChunkChanged.forEach(event -> event.invoke(chunk, chunk));
+        BigShotCommonEventStorage.onChunkChanged.forEach(event -> event.invoke(level, chunk, chunk));
     }
 }
