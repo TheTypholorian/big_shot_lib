2c5ecea822c1639a05a945d98a4243144fc1c203
The Typhothanian <thetyphothanian@gmail.com>
2026-02-21 19:49:45 -0700

    3.9.1 yay

---
+++ fabric/src/main/kotlin/net/typho/big_shot_lib/impl/registration/RegisteredObjectImpl.kt
@@ -0,0 +1,18 @@
+package net.typho.big_shot_lib.impl.registration
+
+import net.minecraft.core.Registry
+import net.typho.big_shot_lib.api.registration.RegisteredObject
+import net.typho.big_shot_lib.api.util.resources.NeoResourceKey
+import net.typho.big_shot_lib.api.util.resources.ResourceIdentifier
+
+@JvmRecord
+data class RegisteredObjectImpl<T>(
+    override val registry: NeoResourceKey<Registry<T>>,
+    override val key: ResourceIdentifier,
+    @JvmField
+    val value: T
+) : RegisteredObject<T> {
+    override fun get() = value
+
+    override fun isRegistered() = true
+}
