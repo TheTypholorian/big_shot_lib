2946c25c9b11f94f3efdc0ced36874162a934d79
The Typhothanian <thetyphothanian@gmail.com>
2026-02-22 09:04:17 -0700

    fixed fog buffer yay

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/state/FogUtilImpl.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/state/FogUtilImpl.kt
@@ -1,19 +1,17 @@
 package net.typho.big_shot_lib.impl.state
 
 import com.mojang.blaze3d.systems.RenderSystem
+import net.typho.big_shot_lib.api.client.rendering.buffers.BufferType
 import net.typho.big_shot_lib.api.client.rendering.shaders.GlShader
 import net.typho.big_shot_lib.api.client.rendering.state.FogUtil
 import net.typho.big_shot_lib.mixin.GlBufferAccessor
-import org.lwjgl.opengl.GL30.glBindBufferRange
-import org.lwjgl.opengl.GL31.GL_UNIFORM_BUFFER
 
 class FogUtilImpl : FogUtil {
     override fun upload(shader: GlShader) {
         val mojang = RenderSystem.getShaderFog() ?: return
-        glBindBufferRange(
-            GL_UNIFORM_BUFFER,
-            0, // TODO
-            //glGetUniformBlockIndex((shader as GlNamed).glId(), "Fog"),
+        println("${shader.getUniformBuffer("Fog")?.binding}")
+        shader.getUniformBuffer("Fog")?.setRange(
+            BufferType.UNIFORM_BUFFER,
             (mojang.buffer as GlBufferAccessor).handle,
             mojang.offset.toLong(),
             mojang.length.toLong()
