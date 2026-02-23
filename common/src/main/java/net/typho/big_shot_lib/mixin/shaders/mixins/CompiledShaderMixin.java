c641a4e0e77675005573c557508f332682cc3ac5
The Typhothanian <thetyphothanian@gmail.com>
2026-02-23 13:42:36 -0700

    fixed imports and such

--- common/src/main/java/net/typho/big_shot_lib/mixin/shaders/mixins/CompiledShaderMixin.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/shaders/mixins/CompiledShaderMixin.java
@@ -2,7 +2,7 @@ package net.typho.big_shot_lib.mixin.shaders.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.shaders.CompiledShader;
-import net.typho.big_shot_lib.api.client.rendering.shaders.ShaderSourceType;
+import net.typho.big_shot_lib.api.client.opengl.shaders.ShaderSourceType;
 import net.typho.big_shot_lib.impl.shaders.mixins.ShaderMixinThreadLocal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
