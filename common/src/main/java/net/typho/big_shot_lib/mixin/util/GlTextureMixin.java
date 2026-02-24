1e9b6f28bda0f8f6803048261cbdb30a283589f6
The Typhothanian <thetyphothanian@gmail.com>
2026-02-24 08:08:40 -0700

    fixed GlTextureMixin, removed MeshMixin

--- common/src/main/java/net/typho/big_shot_lib/mixin/util/GlTextureMixin.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/util/GlTextureMixin.java
@@ -1,9 +1,9 @@
 package net.typho.big_shot_lib.mixin.util;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.opengl.GlTexture;
 import com.mojang.blaze3d.textures.GpuTexture;
 import com.mojang.blaze3d.textures.TextureFormat;
-import it.unimi.dsi.fastutil.ints.Int2IntFunction;
 import net.minecraft.client.Minecraft;
 import net.typho.big_shot_lib.api.client.opengl.state.GlStateStack;
 import net.typho.big_shot_lib.api.client.opengl.util.OpenGL;
@@ -13,7 +13,6 @@ import net.typho.big_shot_lib.impl.util.DynamicBufferRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -30,39 +29,32 @@ public abstract class GlTextureMixin extends GpuTexture {
         super(p_404771_, p_405873_, p_405456_, p_405638_, p_404958_, p_419943_, p_423664_);
     }
 
-    @ModifyArg(
-            method = "getFbo",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lit/unimi/dsi/fastutil/ints/Int2IntMap;computeIfAbsent(ILit/unimi/dsi/fastutil/ints/Int2IntFunction;)I",
-                    remap = false
-            )
+    @ModifyReturnValue(
+            method = "createFbo",
+            at = @At("RETURN")
     )
-    private Int2IntFunction getFbo(Int2IntFunction func) {
-        return j -> {
-            int glId = func.get(j);
-            GlTexture mainFboColor = (GlTexture) Minecraft.getInstance().getMainRenderTarget().getColorTexture();
+    private int getFbo(int glId) {
+        GlTexture mainFboColor = (GlTexture) Minecraft.getInstance().getMainRenderTarget().getColorTexture();
 
-            if (mainFboColor != null && mainFboColor.glId() == this.glId()) {
-                GlStateStack.framebuffer.push(glId);
+        if (mainFboColor != null && mainFboColor.glId() == this.glId()) {
+            GlStateStack.framebuffer.push(glId);
 
-                List<Integer> buffers = new LinkedList<>();
-                buffers.add(GL_COLOR_ATTACHMENT0);
-                int i = GL_COLOR_ATTACHMENT1;
+            List<Integer> buffers = new LinkedList<>();
+            buffers.add(GL_COLOR_ATTACHMENT0);
+            int i = GL_COLOR_ATTACHMENT1;
 
-                for (DynamicBuffer<?> buffer : DynamicBufferRegistry.buffers) {
-                    int point = i++;
-                    buffers.add(point);
-                    buffer.resize(getWidth(0), getHeight(0), BufferUploader::uploadNull);
-                    buffer.attachToFramebuffer(point);
-                }
-
-                OpenGL.INSTANCE.drawBuffers(buffers.stream().mapToInt(k -> k).toArray());
-
-                GlStateStack.framebuffer.pop();
+            for (DynamicBuffer<?> buffer : DynamicBufferRegistry.buffers) {
+                int point = i++;
+                buffers.add(point);
+                buffer.resize(getWidth(0), getHeight(0), BufferUploader::uploadNull);
+                buffer.attachToFramebuffer(point);
             }
 
-            return glId;
-        };
+            OpenGL.INSTANCE.drawBuffers(buffers.stream().mapToInt(k -> k).toArray());
+
+            GlStateStack.framebuffer.pop();
+        }
+
+        return glId;
     }
 }
