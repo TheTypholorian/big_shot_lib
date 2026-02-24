8dd3329f993d3495f05d0ecccb779e1b9d4d6f8c
The Typhothanian <thetyphothanian@gmail.com>
2026-02-23 20:18:43 -0700

    new fog.glsl method

--- common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
+++ common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
@@ -6,5 +6,9 @@ uniform int FogShape;
 uniform vec4 FogColor;
 
 vec4 bigShotFog(vec4 inColor, vec3 delta) {
-    return linear_fog(inColor, fog_distance(delta, FogShape), FogStart, FogEnd, FogColor);
+    return bigShotFog(inColor, delta, FogColor);
+}
+
+vec4 bigShotFog(vec4 inColor, vec3 delta, vec4 fogColor) {
+    return linear_fog(inColor, fog_distance(delta, FogShape), FogStart, FogEnd, fogColor);
 }
