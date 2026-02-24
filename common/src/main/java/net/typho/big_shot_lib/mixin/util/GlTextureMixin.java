7358aff9c69d59f3902ac1033a70cae80b60c808
The Typhothanian <thetyphothanian@gmail.com>
2026-02-24 06:23:14 -0700

    fixed mixin imports

--- common/src/main/java/net/typho/big_shot_lib/mixin/util/GlTextureMixin.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/util/GlTextureMixin.java
@@ -5,11 +5,11 @@ import com.mojang.blaze3d.textures.GpuTexture;
 import com.mojang.blaze3d.textures.TextureFormat;
 import it.unimi.dsi.fastutil.ints.Int2IntFunction;
 import net.minecraft.client.Minecraft;
-import net.typho.big_shot_lib.api.client.rendering.buffers.DynamicBuffer;
-import net.typho.big_shot_lib.api.client.rendering.buffers.DynamicBufferRegistry;
-import net.typho.big_shot_lib.api.client.rendering.state.GlStateStack;
-import net.typho.big_shot_lib.api.client.rendering.state.OpenGL;
+import net.typho.big_shot_lib.api.client.opengl.state.GlStateStack;
+import net.typho.big_shot_lib.api.client.opengl.util.OpenGL;
+import net.typho.big_shot_lib.api.client.util.dynamic_buffers.DynamicBuffer;
 import net.typho.big_shot_lib.api.util.buffers.BufferUploader;
+import net.typho.big_shot_lib.impl.util.DynamicBufferRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +17,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import static org.lwjgl.opengl.GL30.GL_COLOR_ATTACHMENT0;
+import static org.lwjgl.opengl.GL30.GL_COLOR_ATTACHMENT1;
 
 @Mixin(GlTexture.class)
 public abstract class GlTextureMixin extends GpuTexture {
@@ -48,12 +48,13 @@ public abstract class GlTextureMixin extends GpuTexture {
 
                 List<Integer> buffers = new LinkedList<>();
                 buffers.add(GL_COLOR_ATTACHMENT0);
+                int i = GL_COLOR_ATTACHMENT1;
 
-                for (Map.Entry<Integer, DynamicBuffer> entry : DynamicBufferRegistry.buffers.entrySet()) {
-                    int point = GL_COLOR_ATTACHMENT0 + entry.getKey();
+                for (DynamicBuffer<?> buffer : DynamicBufferRegistry.buffers) {
+                    int point = i++;
                     buffers.add(point);
-                    entry.getValue().resize(getWidth(0), getHeight(0), BufferUploader::uploadNull);
-                    entry.getValue().attachToFramebuffer(point);
+                    buffer.resize(getWidth(0), getHeight(0), BufferUploader::uploadNull);
+                    buffer.attachToFramebuffer(point);
                 }
 
                 OpenGL.INSTANCE.drawBuffers(buffers.stream().mapToInt(k -> k).toArray());
