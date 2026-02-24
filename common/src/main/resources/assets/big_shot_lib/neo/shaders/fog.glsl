fc56761375ba8b2920701907be0eb15ef62d1f94
The Typhothanian <thetyphothanian@gmail.com>
2026-02-23 20:18:43 -0700

    new fog.glsl method

--- common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
+++ common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
@@ -1,5 +1,9 @@
 #include "minecraft:fog.glsl"
 
 vec4 bigShotFog(vec4 inColor, vec3 delta) {
-    return apply_fog(inColor, fog_spherical_distance(delta), fog_cylindrical_distance(delta), FogEnvironmentalStart, FogEnvironmentalEnd, FogRenderDistanceStart, FogRenderDistanceEnd, FogColor);
+    return bigShotFog(inColor, delta, FogColor);
+}
+
+vec4 bigShotFog(vec4 inColor, vec3 delta, vec4 fogColor) {
+    return apply_fog(inColor, fog_spherical_distance(delta), fog_cylindrical_distance(delta), FogEnvironmentalStart, FogEnvironmentalEnd, FogRenderDistanceStart, FogRenderDistanceEnd, fogColor);
 }
