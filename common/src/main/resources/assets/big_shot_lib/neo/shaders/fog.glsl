2527f29f96d6382e387c13aae3f584e5b993d140
The Typhothanian <thetyphothanian@gmail.com>
2026-02-24 07:05:16 -0700

    fixed fog.glsl

--- common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
+++ common/src/main/resources/assets/big_shot_lib/neo/shaders/fog.glsl
@@ -1,9 +1,9 @@
 #include "minecraft:fog.glsl"
 
-vec4 bigShotFog(vec4 inColor, vec3 delta) {
-    return bigShotFog(inColor, delta, FogColor);
-}
-
 vec4 bigShotFog(vec4 inColor, vec3 delta, vec4 fogColor) {
     return apply_fog(inColor, fog_spherical_distance(delta), fog_cylindrical_distance(delta), FogEnvironmentalStart, FogEnvironmentalEnd, FogRenderDistanceStart, FogRenderDistanceEnd, fogColor);
+}
+
+vec4 bigShotFog(vec4 inColor, vec3 delta) {
+    return bigShotFog(inColor, delta, FogColor);
 }
