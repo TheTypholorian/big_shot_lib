3bd3221c1bd4a792820e0835af518f6d0840be49
The Typhothanian <thetyphothanian@gmail.com>
2026-03-05 06:35:46 -0700

    fixed problem with NeoAtlasImpl calling atlas.getId() which is required to be on render thread

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/util/NeoAtlasImpl.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/util/NeoAtlasImpl.kt
@@ -2,8 +2,10 @@ package net.typho.big_shot_lib.impl.util
 
 import net.minecraft.client.renderer.texture.TextureAtlas
 import net.typho.big_shot_lib.BigShotLib.toNeo
-import net.typho.big_shot_lib.api.client.opengl.buffers.NeoTexture2D
+import net.typho.big_shot_lib.api.client.opengl.state.GlStateStack
+import net.typho.big_shot_lib.api.client.opengl.util.OpenGL
 import net.typho.big_shot_lib.api.client.opengl.util.TextureFormat
+import net.typho.big_shot_lib.api.client.opengl.util.TextureType
 import net.typho.big_shot_lib.api.client.util.quads.NeoAtlas
 import net.typho.big_shot_lib.api.client.util.quads.NeoAtlasSprite
 import net.typho.big_shot_lib.api.util.buffers.BufferUploader
@@ -14,11 +16,7 @@ import java.util.function.Consumer
 data class NeoAtlasImpl(
     @JvmField
     val atlas: TextureAtlas
-) : NeoTexture2D(
-    atlas.id,
-    TextureFormat.RGBA,
-    false
-), NeoAtlas {
+) : NeoAtlas {
     override val sprites: Map<ResourceIdentifier, NeoAtlasSprite>
         get() = (atlas as TextureAtlasAccessor).texturesByName
             .mapKeys { it.key.toNeo() }
@@ -31,8 +29,37 @@ data class NeoAtlasImpl(
         get() = (atlas as TextureAtlasAccessor).mipLevel
     override val location: ResourceIdentifier
         get() = atlas.location().toNeo()
+    override val format: TextureFormat
+        get() = TextureFormat.RGBA
+    override val type: TextureType
+        get() = TextureType.TEXTURE_2D
+    override val glId: Int
+        get() = atlas.id
+
+    override fun attachToFramebuffer(attachment: Int) {
+        OpenGL.INSTANCE.attachFramebufferTexture2D(attachment, type, glId)
+    }
 
     override fun resize(width: Int, height: Int, upload: Consumer<BufferUploader>) {
         throw UnsupportedOperationException("Cannot resize vanilla texture atlas ${atlas.location()}")
+    }
+
+    override fun bind(pushStack: Boolean) {
+        if (pushStack) {
+            GlStateStack.textures[type]!!.push(glId)
+        } else {
+            GlStateStack.textures[type]!!.bind.accept(glId)
+        }
+    }
+
+    override fun unbind(popStack: Boolean) {
+        if (popStack) {
+            GlStateStack.textures[type]!!.pop()
+        } else {
+            GlStateStack.textures[type]!!.bind.accept(0)
+        }
+    }
+
+    override fun free() {
     }
 }
