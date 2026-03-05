348d3157df5cc673b143b727da160fc23d131e55
The Typhothanian <thetyphothanian@gmail.com>
2026-03-04 21:24:48 -0700

    3.15.0 impl

--- common/src/main/kotlin/net/typho/big_shot_lib/impl/util/BlockUtilImpl.kt
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/util/BlockUtilImpl.kt
@@ -1,8 +1,14 @@
 package net.typho.big_shot_lib.impl.util
 
+import net.minecraft.client.renderer.ItemBlockRenderTypes
+import net.minecraft.client.renderer.RenderType
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
+import net.typho.big_shot_lib.api.client.util.BlockRenderSettings
 import net.typho.big_shot_lib.api.util.BlockUtil
 import org.joml.Vector3f
 
@@ -21,5 +27,29 @@ class BlockUtilImpl : BlockUtil {
         level: Level
     ): Vector3f {
         return state.getOffset(pos).toVector3f()
+    }
+
+    override fun getBlockSettings(state: BlockState): BlockRenderSettings {
+        return when (ItemBlockRenderTypes.getChunkRenderType(state)) {
+            RenderType.solid() -> BlockRenderSettings.SOLID
+            RenderType.cutout(), RenderType.cutoutMipped() -> BlockRenderSettings.CUTOUT
+            RenderType.translucent(), RenderType.translucentMovingBlock() -> BlockRenderSettings.TRANSLUCENT
+            RenderType.tripwire() -> BlockRenderSettings.TRIPWIRE
+            else -> BlockRenderSettings.SOLID
+        }
+    }
+
+    override fun shouldRenderFace(
+        level: BlockGetter,
+        pos: BlockPos,
+        direction: Direction
+    ): Boolean {
+        return Block.shouldRenderFace(
+            level.getBlockState(pos),
+            level,
+            pos.relative(direction),
+            direction,
+            pos
+        )
     }
 }
---
+++ common/src/main/kotlin/net/typho/big_shot_lib/impl/util/NeoBakedQuadImpl.kt
@@ -0,0 +1,89 @@
+package net.typho.big_shot_lib.impl.util
+
+import com.mojang.blaze3d.vertex.DefaultVertexFormat
+import net.minecraft.client.renderer.block.model.BakedQuad
+import net.minecraft.core.Direction
+import net.typho.big_shot_lib.BigShotLib.toNeo
+import net.typho.big_shot_lib.api.client.opengl.buffers.NeoVertexConsumer
+import net.typho.big_shot_lib.api.client.opengl.util.TextureUtil
+import net.typho.big_shot_lib.api.client.util.quads.NeoAtlasSprite
+import net.typho.big_shot_lib.api.client.util.quads.NeoBakedQuad
+import net.typho.big_shot_lib.api.client.util.quads.NeoVertexData
+import net.typho.big_shot_lib.api.util.IColor
+import org.joml.*
+import org.lwjgl.system.MemoryStack
+
+data class NeoBakedQuadImpl(
+    @JvmField
+    val quad: BakedQuad
+) : NeoBakedQuad {
+    override val vertices: Array<NeoVertexData> = arrayOf(
+        VertexDataImpl(0),
+        VertexDataImpl(8),
+        VertexDataImpl(16),
+        VertexDataImpl(24)
+    )
+    override val tintIndex: Int?
+        get() = if (quad.isTinted) quad.tintIndex else null
+    override val direction: Direction?
+        get() = quad.direction
+    override val sprite: NeoAtlasSprite
+        get() = NeoAtlasSpriteImpl(TextureUtil.INSTANCE.getAtlas(quad.sprite.atlasLocation().toNeo()), quad.sprite)
+    override val shade: Boolean
+        get() = quad.isShade
+
+    inner class VertexDataImpl(
+        @JvmField
+        val offset: Int
+    ) : NeoVertexData {
+        override val pos: Vector3fc
+            get() = Vector3f(
+                Float.fromBits(quad.vertices[offset]),
+                Float.fromBits(quad.vertices[offset + 1]),
+                Float.fromBits(quad.vertices[offset + 2]),
+            )
+        override val color: IColor
+            get() = IColor.RGBA(quad.vertices[offset + 3])
+        override val textureUV: Vector2fc
+            get() = Vector2f(
+                Float.fromBits(quad.vertices[offset + 4]),
+                Float.fromBits(quad.vertices[offset + 5])
+            )
+        override val overlayUV: Vector2ic
+            get() {
+                val packed = quad.vertices[offset + 6]
+                return Vector2i(
+                    packed ushr 16,
+                    packed and 0xFFFF
+                )
+            }
+        override val lightUV: Vector2ic? = null
+        override val normal: Vector3fc
+            get() {
+                val packed = quad.vertices[offset + 7]
+                return Vector3f(
+                    (packed ushr 24).toByte() / 127f,
+                    ((packed ushr 16) and 0xFF).toByte() / 127f,
+                    ((packed ushr 8) and 0xFF).toByte() / 127f
+                )
+            }
+
+        override fun put(consumer: NeoVertexConsumer) {
+            MemoryStack.stackPush().use { stack ->
+                val buffer = stack.malloc(DefaultVertexFormat.BLOCK.vertexSize)
+
+                buffer.asIntBuffer().put(quad.vertices, offset, 8)
+
+                consumer.vertex(buffer.getFloat(0), buffer.getFloat(4), buffer.getFloat(8))
+                consumer.color(IColor.RGBA(buffer.getInt(12)))
+                consumer.textureUV(buffer.getFloat(16), buffer.getFloat(20))
+                consumer.overlayUV(buffer.getInt(24))
+                consumer.normal(
+                    buffer.get(28) / 127f,
+                    buffer.get(29) / 127f,
+                    buffer.get(30) / 127f
+                )
+            }
+        }
+    }
+}
