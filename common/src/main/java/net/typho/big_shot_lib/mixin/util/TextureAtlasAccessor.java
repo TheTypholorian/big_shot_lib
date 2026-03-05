4662b95e46977548ad873531c3b84f64a2f08d9d
The Typhothanian <thetyphothanian@gmail.com>
2026-03-04 21:24:48 -0700

    3.15.0 impl

--- common/src/main/java/net/typho/big_shot_lib/mixin/util/TextureAtlasAccessor.java
+++ common/src/main/java/net/typho/big_shot_lib/mixin/util/TextureAtlasAccessor.java
@@ -1,8 +1,12 @@
 package net.typho.big_shot_lib.mixin.util;
 
 import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
 
 @Mixin(TextureAtlas.class)
 public interface TextureAtlasAccessor {
@@ -11,4 +15,10 @@ public interface TextureAtlasAccessor {
 
     @Accessor("height")
     int getHeight();
+
+    @Accessor("mipLevel")
+    int getMipLevel();
+
+    @Accessor("texturesByName")
+    Map<ResourceLocation, TextureAtlasSprite> getTexturesByName();
 }
