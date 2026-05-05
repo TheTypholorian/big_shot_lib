cb327b4af43febcd09a095d896e783636161622d
The Typhothanian <thetyphothanian@gmail.com>
2026-05-05 06:14:23 -0600

    added CLAMP_TO_BORDER to GlTextureWrapMode

--- api/src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/opengl/constant/GlTextureWrapMode.kt
+++ api/src/main/kotlin/net/typho/big_shot_lib/api/client/rendering/opengl/constant/GlTextureWrapMode.kt
@@ -2,10 +2,12 @@ package net.typho.big_shot_lib.api.client.rendering.opengl.constant
 
 import org.lwjgl.opengl.GL11.GL_CLAMP
 import org.lwjgl.opengl.GL11.GL_REPEAT
+import org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER
 
 enum class GlTextureWrapMode(
     override val glId: Int
 ) : GlConstant {
     CLAMP(GL_CLAMP),
+    CLAMP_TO_BORDER(GL_CLAMP_TO_BORDER),
     REPEAT(GL_REPEAT)
 }
