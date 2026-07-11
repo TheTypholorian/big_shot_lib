9ece09e732bb74256ea5e14e63779b7a38adfa0c
The Typhothanian <thetyphothanian@gmail.com>
2026-07-11 13:30:02 -0600

    added checking for if the accessor method already exists, and moved the cursor to the accessor method

--- intellij/src/main/kotlin/net/typho/big_shot_lib/intellij/mixin/CreateAccessorAction.kt
+++ intellij/src/main/kotlin/net/typho/big_shot_lib/intellij/mixin/CreateAccessorAction.kt
@@ -1,14 +1,18 @@
 package net.typho.big_shot_lib.intellij.mixin
 
 import com.demonwav.mcdev.util.addAnnotation
+import com.demonwav.mcdev.util.constantStringValue
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElementFactory
 import com.intellij.psi.PsiField
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiModifier
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
 import com.intellij.psi.search.GlobalSearchScope
@@ -17,6 +21,10 @@ import org.jetbrains.uast.UField
 import org.jetbrains.uast.toUElement
 
 class CreateAccessorAction : AnAction() {
+    companion object {
+        const val ACCESSOR_CLASS = "org.spongepowered.asm.mixin.gen.Accessor"
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         val element = e.getData(CommonDataKeys.PSI_ELEMENT) as? PsiField ?: return
         val target = PsiTreeUtil.getParentOfType(element, PsiClass::class.java) ?: return
@@ -31,20 +39,34 @@ class CreateAccessorAction : AnAction() {
             else -> TODO("prompt for which mixin")
         }
 
-        WriteCommandAction.runWriteCommandAction(project) {
-            val factory = PsiElementFactory.getInstance(project)
-
-            val method = factory.createMethod(
-                $$"$${project.name}$get$${element.name[0].uppercase()}$${element.name.substring(1)}",
-                element.type
-            )
-            method.body?.delete()
-            val annotation = factory.createAnnotationFromText("@org.spongepowered.asm.mixin.gen.Accessor(\"${element.name}\")", mixin)
-            JavaCodeStyleManager.getInstance(project).shortenClassReferences(annotation)
-            method.addAnnotation(annotation)
-
-            mixin.add(method)
+        for (method in mixin.methods) {
+            if (method.annotations.any { it.qualifiedName == ACCESSOR_CLASS && it.findAttributeValue("value")?.constantStringValue == element.name }) {
+                FileEditorManager.getInstance(project)
+                    .openTextEditor(OpenFileDescriptor(project, mixin.containingFile.virtualFile), true)
+                    ?.caretModel
+                    ?.moveToOffset(method.textRange.startOffset)
+                return
+            }
         }
+        val factory = PsiElementFactory.getInstance(project)
+
+        var method = factory.createMethod(
+            $$"$${project.name}$get$${element.name[0].uppercase()}$${element.name.substring(1)}",
+            element.type
+        )
+        method.body?.delete()
+        val annotation = factory.createAnnotationFromText("@$ACCESSOR_CLASS(\"${element.name}\")", mixin)
+        JavaCodeStyleManager.getInstance(project).shortenClassReferences(annotation)
+        method.addAnnotation(annotation)
+
+        WriteCommandAction.runWriteCommandAction(project) {
+            method = mixin.add(method) as PsiMethod
+        }
+
+        FileEditorManager.getInstance(project)
+            .openTextEditor(OpenFileDescriptor(project, mixin.containingFile.virtualFile), true)
+            ?.caretModel
+            ?.moveToOffset(method.textRange.startOffset)
     }
 
     override fun update(e: AnActionEvent) {
